4ab1bd5cbb37b1b25904e0120e876864f51d3281
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-03-08 00:29:44 +0400

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import "./App.css";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello React </h1>
+      <header className="header">
+        <div className="header-container">
+          <div className="header-logo">
+            <span>ANY LOGO</span>
+          </div>
+          <GiHamburgerMenu className="burger-menu"/>
+          <div className="header-navigation">
+            <nav className="navigation">
+              <ul className="ul-nav">
+                <li>მთავარი</li>
+                <li>ჩვენ შესახებ</li>
+                <li>სიახლეები</li>
+                <li>კონტაქტი</li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
