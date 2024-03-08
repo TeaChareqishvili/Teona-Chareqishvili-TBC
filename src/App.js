d08b5d0fb04a66f33d904fdc2c71d4013559c1d4
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-03-08 13:14:21 +0400

    banner section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 
-
 function App() {
   return (
     <div className="App">
@@ -10,7 +9,7 @@ function App() {
           <div className="header-logo">
             <span>ANY LOGO</span>
           </div>
-          <GiHamburgerMenu className="burger-menu"/>
+          <GiHamburgerMenu className="burger-menu" />
           <div className="header-navigation">
             <nav className="navigation">
               <ul className="ul-nav">
@@ -23,6 +22,18 @@ function App() {
           </div>
         </div>
       </header>
+      <section className="banner-section">
+        <div className="banner">
+          <img src="../image/banner.png" alt="banner" />
+        </div>
+        <div className="banner-content">
+          <h1>კომპანიის სახელი</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
