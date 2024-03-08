794298f567c6784465226385183594425119b4b0
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-03-08 13:58:03 +0400

    abous us content

--- src/App.js
+++ src/App.js
@@ -34,6 +34,26 @@ function App() {
           </p>
         </div>
       </section>
+      <section className="about-us">
+        <div className="about-us-container">
+          <div className="about-us-img">
+            <img src="../image/aboutUs.jpg" alt="company" />
+          </div>
+          <div className="about-us-text">
+            <h2>ჩვენს შესახებ</h2>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          </div>
+         
+        </div>
+      </section>
     </div>
   );
 }
