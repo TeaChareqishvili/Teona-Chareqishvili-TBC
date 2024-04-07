c9caa51569bff4e310b3aa6f48e0236ad18d377f
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-04-07 22:19:26 +0400

    blog styles

--- components/blog/Blog.jsx
+++ components/blog/Blog.jsx
@@ -1,12 +1,20 @@
 import { blogData } from "../projectData";
+import Image from "next/image";
 const Blog = () => {
   return (
-    <div className="blog-wrapper">
+    <div className="flex items-center justify-center flex-col md:flex-row md:flex-wrap p-4">
       {blogData.map((item) => (
-        <div key={item.id} className="blog-item">
-          <div className="blog-image">
+        <div
+          key={item.id}
+          className="mx-auto my-8 w-full self-center md:w-auto"
+        >
+          <div className="w-full max-w-500 mx-auto object-cover cursor-pointer overflow-hidden md:p-10">
             {" "}
-            <img src={item.image} alt={item.title} />
+            <Image
+              className="w-full h-auto object-cover transition duration-400 transform hover:scale-125"
+              src={item.image}
+              alt={item.title}
+            />
           </div>
           <div className="blog-content">
             <h2>{item.title}</h2>
