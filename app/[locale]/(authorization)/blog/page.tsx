b00f96f5c241cbe732c59809ce42c44b95355cb7
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-05-06 23:04:41 +0400

    blogs

--- app/[locale]/(authorization)/blog/page.tsx
+++ app/[locale]/(authorization)/blog/page.tsx
@@ -13,28 +13,27 @@ async function getBlog() {
 export default async function Blog() {
   const blogs = await getBlog();
 
+  console.log(blogs, "blogs");
   return (
-    <div className="flex items-center justify-center flex-col md:flex-row md:flex-wrap p-4 mt-[60px]">
+    <div className="flex items-center justify-center flex-col md:flex-row md:flex-wrap p-4 mt-[80px]">
       {blogs.map((item) => (
         <div
           key={item.id}
-          className="mx-auto my-8 w-full self-center md:w-auto"
+          className=" w-[400px]  mt-[60px] pb-[40px] border  border-red flex flex-col items-center justify-center mr-[15px] bg-[#e2e7e0] rounded-md overflow-hidden transition-all duration-200"
         >
-          <div className="w-full max-w-500 mx-auto object-cover cursor-pointer overflow-hidden md:p-10">
-            {" "}
-            <Image
-              className="w-full h-auto object-cover transition duration-400 transform hover:scale-125"
-              src={blogImage}
-              alt={item.title}
-              width="300"
-              height="300"
-            />
-          </div>
-          <div className="blog-content">
-            <h2>{item.title}</h2>
+          {" "}
+          <Image
+            className="w-full h-[300px] rounded-md mb-[25px] cursor-pointer hover:scale-110 transition-all duration-200"
+            src={blogImage}
+            alt={item.title}
+          />
+          <div className="flex flex-col items-center justify-center mt-[20px] py-[20px] px-[15px] h-[150px]">
+            <h2 className="text-[18px] text-[#4f544f] cursor-pointer mb-[10px]">
+              {item.title}
+            </h2>
             <Link href={`/singleBlog/${item.id}`}>
               {" "}
-              <button>Read More</button>
+              <button className="blog-btn">Read More</button>
             </Link>
           </div>
         </div>
@@ -42,3 +41,4 @@ export default async function Blog() {
     </div>
   );
 }
+// className="blog-content"
