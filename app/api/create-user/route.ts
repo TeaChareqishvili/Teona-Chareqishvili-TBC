1d954090adb68b0c83f68fff15c306ecdc43964f
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-05-08 03:07:21 +0400

    ..

--- app/api/create-user/route.ts
+++ app/api/create-user/route.ts
@@ -3,9 +3,11 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const result =
-      await sql`CREATE TABLE IF NOT EXISTS user ( id SERIAL, name varchar(255), email varchar(255), age INTEGER );`;
-    return NextResponse.json({ result }, { status: 200 });
+    const result = await sql`
+      CREATE TABLE user (id SERIAL, name varchar(255), email varchar(255) age INTEGER); 
+    `;
+    const rows = result.rows;
+    return NextResponse.json({ rows }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
