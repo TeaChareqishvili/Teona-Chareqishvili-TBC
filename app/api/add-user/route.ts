dcc262e24d0a4346a253c8faa1dda7176a5ff7a6
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-05-08 21:09:58 +0400

    get user

---
+++ app/api/add-user/route.ts
@@ -0,0 +1,19 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const name = searchParams.get("name");
+  const email = searchParams.get("email");
+  const age = searchParams.get("age");
+  try {
+    if (!name || !email || !age)
+      throw new Error(" names, email and age is required");
+    await sql`INSERT INTO users (name, email, age) VALUES (${name}, ${email}, ${age});`;
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+
+  const users = await sql`SELECT * FROM users;`;
+  return NextResponse.json({ users }, { status: 200 });
+}
--- helper/userLogin.ts
+++ helper/userLogin.ts
@@ -1,6 +1,6 @@
 export async function handlerLoginUser(username: string, password: string) {
   try {
-    const response = await fetch("http://localhost:3002/api/login", {
+    const response = await fetch("http://localhost:3001/api/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
