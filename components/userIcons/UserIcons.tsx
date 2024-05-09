4813a8406dbf9074f090be277b4b20970ed96af0
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-05-10 00:29:15 +0400

    modal for edit user

--- components/userIcons/UserIcons.tsx
+++ components/userIcons/UserIcons.tsx
@@ -3,6 +3,9 @@
 import { TiUserDelete } from "react-icons/ti";
 import { FaUserEdit } from "react-icons/fa";
 import { deleteUser } from "@/apiUsers";
+import { useState } from "react";
+import EditModal from "../modalEdit/EditModal";
+import { EditUserForm } from "../modalEdit/EditUserForm";
 
 interface UserIconsProps {
   id: number;
@@ -15,10 +18,21 @@ const UserIcons = ({ id }: UserIconsProps) => {
       console.error("Error deleting user:", error);
     }
   };
+
+  const [isModalEditOpen, setModalEditOpen] = useState(false);
+  const handleModalEditOpen = () => {
+    setModalEditOpen(true);
+  };
+  const handleModalEditClose = () => {
+    setModalEditOpen(false);
+  };
   return (
     <div className="flex items-center justify-center">
       <TiUserDelete className="user-icon" onClick={handleDelete} />
-      <FaUserEdit className="user-icon" />
+      <FaUserEdit className="user-icon" onClick={handleModalEditOpen} />
+      <EditModal isOpen={isModalEditOpen} onClose={handleModalEditClose}>
+        <EditUserForm />
+      </EditModal>
     </div>
   );
 };
