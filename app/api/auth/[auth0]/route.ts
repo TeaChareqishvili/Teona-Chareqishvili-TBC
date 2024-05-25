cedfba9a250d69988fe636fecf098790b81fbdd8
TheLastDance <davit.khachaturov.1@iliauni.edu.ge>
2024-05-26 02:43:38 +0400

    adding route handler for database user creation
    - fixed bug with casual authorization in UserProfile
    - added image configuration to next config
    - created route handler for database user addition
    - added redirect to new route handler from auth api

--- app/api/auth/[auth0]/route.ts
+++ app/api/auth/[auth0]/route.ts
@@ -1,10 +1,13 @@
-import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin, handleLogout } from "@auth0/nextjs-auth0";
 
 export const GET = handleAuth({
   login: handleLogin({
-    returnTo: "/profile",
+    returnTo: "/api/create-auth-user",
     authorizationParams: {
       prompt: "login",
     },
   }),
+  logout: handleLogout({
+    returnTo: "/",
+  })
 });
