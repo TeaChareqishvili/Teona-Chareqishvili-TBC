5bb2c48980d57f3699e6ebb54e4752618ccfcdfb
Daduka Khukhunashvili <daduka.xuxunashvili@gmail.com>
2024-05-25 22:50:27 +0400

    Add and style login/logout component with functioning authentication

--- components/logOutButton/LogOutButton.tsx
+++ components/logOutButton/LogOutButton.tsx
@@ -1,24 +1,35 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { handleLogout } from "@/helper/userLogOut";
-
+// import { useRouter } from "next/navigation";
+// import { handleLogout } from "@/helper/userLogOut";
+import { useUser } from '@auth0/nextjs-auth0/client';
 import { HiOutlineLogout } from "react-icons/hi";
-const LogOutButton = () => {
-  const router = useRouter();
+import { IoLogIn } from "react-icons/io5";
+import Link from 'next/link';
 
-  const handleClick = async () => {
-    await handleLogout();
-    localStorage.removeItem("theme");
-    router.push("/logIn");
-  };
+const LogOutButton = () => {
+  // const router = useRouter();
+  const { user } = useUser();
+
+  // const handleClick = async () => {
+  //   await handleLogout();
+  //   localStorage.removeItem("theme");
+  //   router.push("/logIn");
+  // };
 
   return (
     <div>
-      <HiOutlineLogout
-        onClick={handleClick}
-        className="w-[20px] h-[20px] text-[#264653] cursor-pointer dark:text-[#ffffff] "
-      />
+      {user ? (
+        <Link href="/api/auth/logout"><HiOutlineLogout
+          className="w-[20px] h-[20px] text-[#264653] cursor-pointer dark:text-[#ffffff] "
+        /></Link>
+      ) : (
+        <Link href="/api/auth/login">
+          <IoLogIn
+            className="w-[20px] h-[20px] text-[#264653] cursor-pointer dark:text-[#ffffff] "
+          />
+        </Link>
+      )}
     </div>
   );
 };
