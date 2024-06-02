68440412d9471c2fc39407f5d942806775c582ad
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-06-02 16:13:12 +0400

    contact layout

--- app/[locale]/(authorization)/contact/page.tsx
+++ app/[locale]/(authorization)/contact/page.tsx
@@ -1,14 +1,5 @@
-import { ContactForm } from "../../../../components/contactPage/ContactForm";
-import { ContactInformation } from "../../../../components/contactPage/ContactInformation";
-
 const ContactLayout = () => {
-  return (
-    <div className="w-full flex flex-col lg:mt-[150px] mt-[90px] ">
-      {" "}
-      <ContactInformation />
-      <ContactForm />
-    </div>
-  );
+  return <div className="contact-layout"></div>;
 };
 
 export default ContactLayout;
