965016b740375ad38890b3e1073af95b50762ad3
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-06-24 21:16:47 +0400

    Ai styles

--- app/[locale]/(authorization)/openAi/page.tsx
+++ app/[locale]/(authorization)/openAi/page.tsx
@@ -2,7 +2,7 @@ import ChatBot from "@/components/chatbot/ChatBot";
 
 export default function OpenAi() {
   return (
-    <div className="mt-[120px]">
+    <div className="min-h-screen mt-[120px]">
       <ChatBot />
     </div>
   );
--- components/chatbot/ChatBot.tsx
+++ components/chatbot/ChatBot.tsx
@@ -39,46 +39,57 @@ export default function ChatBot() {
   };
 
   return (
-    <div className="max-w-md mx-auto p-6 border border-gray-300 rounded-lg shadow-md">
-      <div className="text-center mb-4">
-        <h1 className="text-2xl font-bold">Ask OpenAI</h1>
-      </div>
-      <form onSubmit={handleSubmit} className="flex flex-col items-center">
-        <textarea
-          value={prompt}
-          onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
-            setPrompt(e.target.value)
-          }
-          className="w-full h-32 p-2 mb-4 border border-gray-300 rounded-md"
-        />
-        <button
-          type="submit"
-          className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-        >
-          Submit
-        </button>
-      </form>
-      <div className="mt-6">
-        <h2 className="text-xl font-semibold">Popular Questions:</h2>
-        <ul className="mt-2">
-          {popularQuestions.map((question, index) => (
-            <li
-              key={index}
-              onClick={() => handleQuestionClick(question)}
-              className="p-2 bg-blue-100 text-black rounded-md mb-2 cursor-pointer hover:bg-blue-200"
+    <div className="flex flex-col px-[30px] py-[40px]">
+      <div className="flex flex-col w-full items-center justify-center lg:flex-row lg:justify-around m-auto">
+        <div className=" md:w-[600px]">
+          <div className="text-center mb-4">
+            <h1 className="text-2xl font-bold text-[#124E66] dark:text-white">
+              Ask chatBot
+            </h1>
+          </div>
+          <form
+            onSubmit={handleSubmit}
+            className="flex lg:w-full flex-col items-center"
+          >
+            <textarea
+              value={prompt}
+              onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+                setPrompt(e.target.value)
+              }
+              className=" bg-[#2E3944] text-white dark:text-white dark:bg-[#212A31] w-[300px] md:w-full h-[300px] p-2 mb-4 border border-gray-300 rounded-md outline-none"
+            />
+            <button
+              type="submit"
+              className="px-4 py-2 w-2/4 dark:bg-[#124E66] dark:text-white  bg-[#2E3944] text-[white] hover:bg-[#748D92] tracking-widest rounded-md transition-all duration-200 dark:hover:bg-[#212A31]"
             >
-              {question}
-            </li>
-          ))}
-        </ul>
+              Send
+            </button>
+          </form>
+        </div>
+        <div className="mt-6 ml-[15px]">
+          <h2 className="text-xl font-semibold text-[#124E66] dark:text-white">
+            Suggested Questions:
+          </h2>
+          <ul className="mt-2">
+            {popularQuestions.map((question, index) => (
+              <li
+                key={index}
+                onClick={() => handleQuestionClick(question)}
+                className="p-2 dark:bg-[#124E66] bg-[#748D92] text-white hover:bg-[#2E3944] dark:text-white uppercase text-[15px] italic dark:hover:bg-[#212A31] transition-all duration-200 rounded-md mb-2 cursor-pointer"
+              >
+                {question}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="mt-6">
-        <h2 className="text-xl font-semibold">Response:</h2>
-        <div className="flex items-start mt-2 p-4 bg-gray-100 rounded-md border border-gray-200">
+      <div className="mt-6 w-[88%] m-auto ">
+        <h2 className="text-xl dark:text-white font-semibold">Response:</h2>
+        <div className="flex items-start mt-2 p-4 bg-gray-100 rounded-md border min-h-[200px] border-gray-200">
           {loading ? (
             <div className="flex items-center justify-center w-full h-32">
               <svg
-                className="animate-spin h-8 w-8 text-blue-500"
+                className="animate-spin h-8 w-8 text-[#124E66]"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -100,8 +111,10 @@ export default function ChatBot() {
             </div>
           ) : (
             <>
-              <BsRobot className="mr-4" />
-              <p className="flex-1">{response}</p>
+              <BsRobot className="mr-4 text-[#124E66] w-[40px] h-[40px]" />
+              <p className="flex-1 dark:text-[#124E66] italic text-[18px]">
+                {response}
+              </p>
             </>
           )}
         </div>
