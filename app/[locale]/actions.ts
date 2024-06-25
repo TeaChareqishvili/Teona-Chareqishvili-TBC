15944ddeb8d38a04012664f6a4fcdfee486246c4
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-06-26 02:10:50 +0400

    ...

--- app/[locale]/actions.ts
+++ app/[locale]/actions.ts
@@ -373,7 +373,7 @@ export async function getSingleBlogAction(id: string) {
     method: "POST",
     body: JSON.stringify({ id }),
   });
-  revalidatePath(`/singleBlog/${id}`);
+  revalidatePath("/");
   const data = await response.json();
 
   return data.response[0];
