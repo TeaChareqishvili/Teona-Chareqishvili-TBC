3477ed28e6122e90e75c83b0950c07b60db211ec
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-06-26 00:03:21 +0400

    add products assets

--- components/blogs/SingleBlogDetails.tsx
+++ components/blogs/SingleBlogDetails.tsx
@@ -1,17 +1,11 @@
 import { SingleBlogData } from "../../app/[locale]/interface";
-// import { getSession } from "@auth0/nextjs-auth0";
+
 import { EditBlog } from "./EditBlog";
 export default async function SingleBlogDetails({
   singleBlog,
 }: {
   singleBlog: SingleBlogData;
 }) {
-  // const session = await getSession();
-  // console.log(session, "ses");
-  // const isAdmin =
-  //   Array.isArray(session?.user?.role) && session?.user?.role.includes("admin");
-  // console.log(isAdmin, "admin?");
-
   const formatDate = (dateString: string): string => {
     const options: Intl.DateTimeFormatOptions = {
       year: "numeric",
@@ -44,7 +38,7 @@ export default async function SingleBlogDetails({
           <p className="dark:text-[white] text-[#2f3c46] text-[16px] mb-[15px] lg:text-[18px] tracking-wider">
             {singleBlog?.description}
           </p>
-          <p className="text-[12px] text-[#d3d3d3] lg:text-[15px]">
+          <p className="text-[12px] text-[#2f3c46] dark:text-[#d3d3d3] lg:text-[15px]">
             <i className="text-[12px] mr-[5px] lg:text-[15px]">Added On:</i>
             {formatDate(singleBlog?.date)}
           </p>
